068dbbedd1d0a1b7d5b38f41eec5278f58afcb00
Christine <christinebergersd@gmail.com>
2020-10-21 20:07:52 -0500

    Added dim and fct models

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,37 @@
+with
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customers as (
+
+    select * from {{ ref('stg_customers') }}
+
+),
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
+joined as (
+
+    select
+
+        orders.order_id,
+        orders.customer_id,
+        sum(payments.amount_cents)/100 as amount
+        
+    from orders
+    left join payments
+        on orders.order_id = payments.order_id
+    group by 1, 2
+
+)
+
+select * from joined
+
