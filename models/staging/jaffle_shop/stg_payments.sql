9e1d31df4d25e55af2b295b671450b215df99b4e
Christine <christinebergersd@gmail.com>
2020-10-21 20:07:25 -0500

    Added staging models

---
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -0,0 +1,25 @@
+with
+
+source as (
+
+    select * from raw.stripe.payment
+
+),
+
+renamed as (
+
+    select
+
+        id as payment_id,
+        order_id,
+        paymentmethod as payment_method,
+        status,
+        amount as amount_cents,
+        created as created_at,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
