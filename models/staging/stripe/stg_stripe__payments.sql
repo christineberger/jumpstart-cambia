6b9d6e375ec6259cd58fa043d5a64ca9ecf45cf6
Christine <christinebergersd@gmail.com>
2020-11-06 03:14:49 -0600

    Added cents_to_dollars macro

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -14,7 +14,7 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount/100 as amount_usd,
+        {{ cents_to_dollars('amount') }} as amount_usd,
         created as created_at,
         _batched_at
 
